ba121818bdf793c1b44a1ef4af82ff6ca71a8cc2
yunusemreyakisan <yunusemreyakisan@outlook.com>
2023-01-15 14:30:40 +0300

    changed Post model class

---
+++ app/src/main/java/com/yakisan/rasathane/model/Posts.java
@@ -0,0 +1,19 @@
+package com.yakisan.rasathane.model;
+
+public class Posts {
+
+   String title, body;
+
+    public Posts(String title, String body) {
+        this.title = title;
+        this.body = body;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+}
